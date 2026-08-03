4f32e4bd39215b8b5ebba1cb7482a4dfcc31bfbd
cas636 <castanojohan24@gmail.com>
2026-08-03 17:58:56 -0500

    Cambios segun documentación

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import Account from './account';
+import { FusionAuthProvider } from '@fusionauth/react-sdk';
+import type { FusionAuthProviderConfig } from '@fusionauth/react-sdk';
 
-import App from './App.tsx'
-import Callback from './components/callback.tsx'
-import './App.css'
+const fusionAuthProviderConfig: FusionAuthProviderConfig = { 
+  redirectUri: 'http://localhost:5173', 
+  postLogoutRedirectUri: 'http://localhost:5173',
+  shouldAutoRefresh: true,
+  shouldAutoFetchUserInfo: true,
+  scope: 'openid email profile offline_access',
+  clientId: '94ef0899-0429-40b6-864f-4b2cb00a595a',
+  serverUrl: 'http://localhost:9011',
+  onRedirect: () => { console.log('Login successful'); }
+};
 
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/account" element={<Account />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+}
+  
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/callback" element={<Callback />} />
-      </Routes>
+      <FusionAuthProvider {...fusionAuthProviderConfig}>
+        <App />
+      </FusionAuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
-)
+  </StrictMode>
+);
